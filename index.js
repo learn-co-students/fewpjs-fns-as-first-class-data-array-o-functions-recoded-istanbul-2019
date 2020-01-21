a704b2033ee4674df9fd50b00a3ad4f09c989c07
Abdulrahman Y. idlbi <adlogi@gmail.com>
2020-01-22 01:41:31 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,45 @@
+function wakeDog(dogName, dogBreed) {
+    const s = `Wake ${dogName} the ${dogBreed}`;
+    console.log(s);
+    return s;
+}
+
+function leashDog(dogName, dogBreed) {
+    const s = `Leash ${dogName} the ${dogBreed}`;
+    console.log(s);
+    return s;
+}
+
+function walkToPark(dogName, dogBreed) {
+    const s = `Walk to the park with ${dogName} the ${dogBreed}`;
+    console.log(s);
+    return s;
+}
+
+function throwFrisbee(dogName, dogBreed) {
+    const s = `Throw the frisbee for ${dogName} the ${dogBreed}`;
+    console.log(s);
+    return s;
+}
+
+function walkHome(dogName, dogBreed) {
+    const s = `Walk home with ${dogName} the ${dogBreed}`;
+    console.log(s);
+    return s;
+}
+
+function unleashDog(dogName, dogBreed) {
+    const s = `Unleash ${dogName} the ${dogBreed}`;
+    console.log(s);
+    return s;
+}
+
+let routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog];
+
+function exerciseDog(dogName, dogBreed) {
+    let result = [];
+    for (let i = 0; i < routine.length; i++) {
+        result.push(routine[i](dogName, dogBreed));
+    }
+    return result;
+}
